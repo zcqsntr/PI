d6ff164fe7e0e2d345f98ccc9e1ced03a925a4eb
Neythen Treloar <ntreloar@amgradmac269.nexus.uwaterloo.ca>
2019-08-20 16:22:21 -0400

    .

---
+++ integrate in simulink/plot_simulink_output.m
@@ -0,0 +1,15 @@
+structure = out.pops;
+time = structure.time;
+populations = extractfield(structure.signals, 'values');
+N1 = squeeze(populations(1, 1, :));
+N2 = squeeze(populations(1, 2, :));
+
+figure
+plot([0:3:1000], N1)
+hold on 
+plot([0:3:1000], N2)
+
+xlabel("Time (hours)")
+ylabel("Population (10^6 cells L^{-1})")
+plot([0 1000], [700, 700], 'Color', 'g')
+plot([0 1000], [250 250], 'Color', 'g')
